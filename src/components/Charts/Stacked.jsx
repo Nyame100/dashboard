683277934bf5b9c89be882a53cf4f4cb617b94a8
Nyame100 <nyamerichmond100@gmail.com>
2022-12-20 02:41:25 +0100

    completing the ecommerce page

--- src/components/Charts/Stacked.jsx
+++ src/components/Charts/Stacked.jsx
@@ -16,8 +16,26 @@ import {
   stackedPrimaryYAxis,
 } from "../../data/dummy";
 
-const Stacked = () => {
-  return <div>Stacked</div>;
+const Stacked = ({ width, height }) => {
+  return (
+    <ChartComponent
+      width={width}
+      height={height}
+      id="charts"
+      primaryXAxis={stackedPrimaryXAxis}
+      primaryYAxis={stackedPrimaryYAxis}
+      chartArea={{ border: { width: 0 } }}
+      tooltip={{ enable: true }}
+      legendSettings={{ background: "white" }}
+    >
+      <Inject services={[Legend, Category, StackingColumnSeries, Tooltip]} />
+      <SeriesCollectionDirective>
+        {stackedCustomSeries.map((item, index) => {
+          return <SeriesDirective key={index} {...item} />;
+        })}
+      </SeriesCollectionDirective>
+    </ChartComponent>
+  );
 };
 
 export default Stacked;
